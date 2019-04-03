616e8538f218c546b16aa07a8c2a1b56801b4b26
Emmett <Ehasley1136@stu.d214.org>
2019-04-03 11:19:06 -0500

    Added Map View and ability to zoom in on current location

--- ZaHunter/ViewController.swift
+++ ZaHunter/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+	@IBOutlet weak var mapView: MKMapView!
+	let locationManager = CLLocationManager()
+	var location : CLLocation!
+	var za : [MKMapItem] = []
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		locationManager.requestWhenInUseAuthorization()
+		mapView.showsUserLocation = true
+		locationManager.delegate = self
+		mapView.delegate = self
+		locationManager.desiredAccuracy = kCLLocationAccuracyBest
+		locationManager.startUpdatingLocation()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		location = locations[0]
+	}
+	@IBAction func whenZoom(_ sender: Any) {
+		let center = location.coordinate
+		let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+		let region = MKCoordinateRegion(center: center, span: span)
+		mapView.setRegion(region, animated: true)
+	}
+	
 
 }
 
